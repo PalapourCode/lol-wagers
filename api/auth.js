dc2d1a3d26b722576035fbe21405b8cde94a1994
PalapourCode <maxime.ardisson.btsci@gmail.com>
2026-02-21 23:02:22 +0100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,6 @@
 const { neon } = require("@neondatabase/serverless");
 
-const sql = neon(process.env.STORAGE_URL);
+const sql = neon(process.env.POSTGRES_URL);
 
 async function initDB() {
   await sql`
